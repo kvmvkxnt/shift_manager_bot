7a8010a28db15e90a3ca8331ef3a0b702c812795
kvmvkxnt <56192327+kvmvkxnt@users.noreply.github.com>
2026-03-01 00:35:22 +0400

    Created basic states.

---
+++ src/shift_manager_bot/bot/states.py
@@ -0,0 +1,15 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class CreateShiftStates(StatesGroup):
+    waiting_for_date = State()
+    waiting_for_time = State()
+    waiting_for_max_employees = State()
+    waiting_for_note = State()
+
+
+class CreateTaskStates(StatesGroup):
+    waiting_for_title = State()
+    waiting_for_desciption = State()
+    waiting_for_employee = State()
+    waiting_for_deadline = State()
